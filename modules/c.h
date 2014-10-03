cb4ece45673602f30a767e888ab7fa3b85fc0fed
Haneef Mubarak <haneef503@gmail.com>
2014-10-02 19:18:54 -0700

    added libc/string

--- modules/c.h
+++ modules/c.h
@@ -161,3 +161,38 @@ struct cmods_libc_io {
 	int	(*vfscanf)	(FILE *, const char *, va_list);
 	int	(*vsscanf)	(const char *, const char *, va_list);
 };
+
+//	strings
+#include <string.h>
+
+struct cmod_libc_string {
+	// general
+	size_t	(*strlen)	(const char *);
+	void*	(*memset)	(void *, int, size_t);
+	char*	(*strcat)	(char *, const char *);
+	char*	(*strncat)	(char *, const char *, size_t);
+	char*	(*strerror)	(int);
+	size_t	(*strxfrm)	(char *, const char *, size_t);
+
+	// copying
+	void*	(*memcpy)	(void *, const void *, size_t);
+	char*	(*strcpy)	(char *, const char *);
+	char*	(*strncpy)	(char *, const char *, size_t);
+	void*	(*memmove)	(void *, const void *, size_t);
+
+	// comparison
+	int	(*memcmp)	(const void *, const void *, size_t);
+	int	(*strcmp)	(const char *, const char *);
+	int	(*strncmp)	(const char *, const char *, size_t);
+	int	(*strcoll)	(const char *, const char *);
+
+	// searching
+	void*	(*memchr)	(const void *, int, size_t);
+	char*	(*strchr)	(const char *, int);
+	char*	(*strrchr)	(const char *, int);
+	size_t	(*strcspn)	(const char *, const char *);
+	size_t	(*strspn)	(const char *, const char *);
+	char*	(*strpbrk)	(const char *, const char *);
+	char*	(*strstr)	(const char *, const char *);
+	char*	(*strtok)	(char *, const char);
+};
